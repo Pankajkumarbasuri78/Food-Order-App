9d23cfce43ed529edec52ecc1f43e952e543c8bf
Pankaj Basuri <pbasuri10078@gmail.com>
2022-09-15 02:31:20 +0530

    card component done

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './MealItem.module.css'
+import { MealItemForm } from './MealItemForm'
 
 
 export const MealItem = (props) => {
@@ -14,7 +15,7 @@ export const MealItem = (props) => {
            <div className={classes.price}>{price}</div>
         </div>
         <div>
-
+            <MealItemForm />
         </div>
     </li>
   )
